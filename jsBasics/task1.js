58650d95aa70ae76e7e9797fcaa889143a291f46
Andr Pi <andr.podo@gmail.com>
2025-01-16 22:32:20 +0200

    task1

---
+++ jsBasics/task1.js
@@ -0,0 +1,12 @@
+let string='Hello!'; // string 
+let number = 100; // number
+let boolean= true; // boolean
+let null_var=null; // null
+let undef=undefined; //undefined
+
+
+console.log(string);
+console.log(number);
+console.log(boolean);
+console.log(null_var);
+console.log(undef);
