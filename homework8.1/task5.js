3a56fe2cd1ea21e4a3a8d2fc5f7311f35906bbb8
Andr Pi <andr.podo@gmail.com>
2025-02-02 17:02:45 +0200

    homework8.1

---
+++ homework8.1/task5.js
@@ -0,0 +1,7 @@
+const firstArray = [1, 2, 3, 4];
+const secondArray = [5, 6, 7, 8];
+
+// Використання оператора розширення для об'єднання масивів
+const mergedArray = [...firstArray, ...secondArray];
+
+console.log(mergedArray);
